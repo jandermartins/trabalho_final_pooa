73cf9d79fe6091d6d274a213e698e0873c8b27de
Jander Martins <jandermartins@alu.ufc.br>
2026-06-16 12:37:48 -0300

    Delete trabalho_final directory

--- trabalho_final/src/main/java/br/ce/crateus/fpo/trabalho_final/exceptions/RecursoNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package br.ce.crateus.fpo.trabalho_final.exceptions;
-
-import org.springframework.data.crossstore.ChangeSetPersister;
-
-public class RecursoNaoEncontradoException extends ChangeSetPersister.NotFoundException {
-    public RecursoNaoEncontradoException(String clienteNãoEncontrado) {
-    }
-}
