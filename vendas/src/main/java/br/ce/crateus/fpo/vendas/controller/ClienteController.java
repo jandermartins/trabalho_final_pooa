e27d2eb34cca86a366666fbb28669cd73ff9952f
dev-davimarques <davimarques10.dm@icloud.com>
2026-06-16 23:03:37 -0300

    fix: RequestMapping com /api

--- vendas/src/main/java/br/ce/crateus/fpo/vendas/controller/ClienteController.java
+++ vendas/src/main/java/br/ce/crateus/fpo/vendas/controller/ClienteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/api/clientes")
 @AllArgsConstructor
 @Tag(name = "Clientes", description = "Endpoints para gerenciamento de clientes")
 public class ClienteController {
--- vendas/src/main/java/br/ce/crateus/fpo/vendas/controller/ProdutoController.java
+++ vendas/src/main/java/br/ce/crateus/fpo/vendas/controller/ProdutoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/produtos")
+@RequestMapping("/api/produtos")
 @AllArgsConstructor
 @Tag(name = "Produtos", description = "Endpoints para gerenciamento de produtos")
 public class ProdutoController {
